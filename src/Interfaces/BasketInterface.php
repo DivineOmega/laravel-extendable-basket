c80bc7202b16afc89c7330c17faf84fe2cc5029c
Jordan Hall <jordan.hall@rapidweb.biz>
2017-09-21 11:51:57 +0100

    Add items method to basket interface

--- src/Interfaces/BasketInterface.php
+++ src/Interfaces/BasketInterface.php
@@ -7,6 +7,7 @@ interface BasketInterface
 {
     public static function getCurrent();
     public static function getNew();
+    public function items();
     public function add(int $quantity, BasketableModel $basketable);
     public function getSubtotal();
     public function getTotalNumberOfItems();
