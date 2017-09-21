b39719c608bbdb7ca40dd978ca4f245bf7bd3181
Jordan Hall <jordan.hall@rapidweb.biz>
2017-09-21 10:04:35 +0100

    Add register method

--- src/Providers/LaravelExtendableBasketServiceProvider.php
+++ src/Providers/LaravelExtendableBasketServiceProvider.php
@@ -5,6 +5,11 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelExtendableBasketServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        
+    }
+
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
