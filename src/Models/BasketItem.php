869d3b6d9b26c165c74cbfcf7327ca8e1c0ba02b
Jordan Hall <jordan.hall@rapidweb.biz>
2017-09-21 13:15:24 +0100

    Add setQuantity method

--- src/Models/BasketItem.php
+++ src/Models/BasketItem.php
@@ -11,6 +11,16 @@ abstract class BasketItem extends Model implements BasketItemInterface
         return $this->morphTo();
     }
 
+    public function setQuantity(int $quantity)
+    {
+        if ($quantity > 0) {
+            $this->quantity = $quantity;
+            $this->save();
+        } else {
+            $this->delete();
+        }
+    }
+
     public function getPrice()
     {
         return $this->quantity * $this->basketable->getPrice();
