c832a9e23b11b17225016e7ee723b67d29db09e9
Jordan Hall <jordan@hall05.co.uk>
2019-02-28 09:47:19 +0000

    Add meta field migration

---
+++ src/Database/Migrations/2019_02_28_094501_add_meta_basket_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBasketItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('basket_items', function (Blueprint $table) {
+            $table->text('meta');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('basket_items', function (Blueprint $table) {
+            $table->dropColumn('meta');
+        });
+    }
+}
