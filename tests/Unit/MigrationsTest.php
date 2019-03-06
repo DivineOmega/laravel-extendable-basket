61ea444aa2ce248e86adc0da869fbe021356de8d
Jordan Hall <jordan@hall05.co.uk>
2019-03-06 16:32:54 +0000

    Test migrations

---
+++ tests/Unit/MigrationsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DivineOmega\LaravelExtendableBasket\Tests\Unit;
+
+use DivineOmega\LaravelExtendableBasket\Interfaces\BasketInterface;
+use DivineOmega\LaravelExtendableBasket\Tests\Models\Basket;
+use DivineOmega\LaravelExtendableBasket\Tests\TestCase;
+use Illuminate\Support\Facades\Schema;
+
+class MigrationsTest extends TestCase
+{
+    /**
+     * Tests that migrations work as expected.
+     */
+    public function testMigrations()
+    {
+        $this->artisan('migrate:fresh', ['--database' => 'testbench'])->run();
+        $this->loadMigrationsFrom(__DIR__ . '/Database/Migrations');
+
+        $this->assertTrue(Schema::hasTable('baskets'));
+        $this->assertTrue(Schema::hasTable('basket_items'));
+
+        $this->assertEquals(
+            ['id', 'created_at', 'updated_at'],
+            Schema::getColumnListing('baskets')
+        );
+
+        $this->assertEquals(
+            ['id', 'basket_id', 'basketable_id', 'basketable_type',
+                'quantity', 'created_at', 'updated_at', 'meta'],
+            Schema::getColumnListing('basket_items')
+        );
+    }
+
+}
